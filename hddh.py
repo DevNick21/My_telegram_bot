8834e084a6e115d9ea37e7f2b8be6df0893912f6
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2023-12-29 19:51:26 +0100

    Making Public my Telegram bot

--- hddh.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime as dt
-
-year = int(dt.now().strftime("%Y"))
-month = int(dt.now().strftime("%m"))
-if month < 5:
-    year += 1
-year = str(year)
-print((year))
--- sending_message.py
+++ sending_message.py
@@ -10,12 +10,13 @@ CHAT_ID = os.getenv("CHAT_ID")
 class Message():
     def __init__(self, message):
         self.send_message(message)
+        pass
 
     def send_message(self, message):
         token = TOKEN
         chat_id = CHAT_ID
         url_req = "https://api.telegram.org/bot" + token + \
-            "/sendMessage" + "?chat_id=" + chat_id + "&text=" + message
+            f"/sendMessage?chat_id={chat_id}&text={message}"
         res = requests.get(url_req)
         res.raise_for_status()
         print(res.text)
